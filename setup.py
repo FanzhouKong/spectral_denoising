64030d5cf2475b8d93e59e9366af8484211ff895
//Fanzhou Kong <36810889+FanzhouKong@users.noreply.github.com>
2024-10-01 16:15:12 -0700

    updated readthedoc file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 DESCRIPTION = 'Spectral denoising and denoising search'
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 with open("README.md", "r") as file:
