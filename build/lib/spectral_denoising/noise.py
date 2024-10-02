b01a54745a1343d1d352afeef3de2b2e0a7479b9
//Fanzhou Kong <36810889+FanzhouKong@users.noreply.github.com>
2024-10-01 18:07:25 -0700

    updated rtd

--- build/lib/spectral_denoising/noise.py
+++ build/lib/spectral_denoising/noise.py
@@ -1,6 +1,6 @@
 import random
 import numpy as np
-from .spectral_operations import standardize_spectra, sort_spectra, add_spectra,normalize_spectra, pack_spectra
+from .spectral_operations import standardize_spectrum, sort_spectrum, add_spectra,normalize_spectrum, pack_spectrum
 def generate_noise(pmz, lamda, n = 100):
     if int(n)!= n:
         n = np.int64(np.ceil(n))
@@ -14,11 +14,11 @@ def generate_noise(pmz, lamda, n = 100):
     # Generating Poisson-distributed random variables
     intensity = np.random.poisson(lam=lamda, size=n)
     intensity = intensity/100
-    return(pack_spectra(mass, intensity))
+    return(pack_spectrum(mass, intensity))
 def add_noise(msms, noise):
-    msms = standardize_spectra(msms)
+    msms = standardize_spectrum(msms)
     msms_c = add_spectra(msms, noise)
-    return(sort_spectra(normalize_spectra(msms_c)) )
+    return(sort_spectrum(normalize_spectrum(msms_c)) )
 def generate_chemical_noise(pmz, lamda, polarity,formula_db,n = 100):
     mass_e =  -0.00054858026
     if polarity =='+':
@@ -47,4 +47,4 @@ def generate_chemical_noise(pmz, lamda, polarity,formula_db,n = 100):
     # Generating Poisson-distributed random variables
     intensity = np.random.poisson(lam=lamda, size=n)
     intensity = intensity/100
-    return(pack_spectra(mass, intensity))
+    return(pack_spectrum(mass, intensity))
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,9 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+import os
+import sys
+sys.path.append('..')
 project = 'Spectral denoising documentation'
 copyright = '2024, Fanzhou Kong'
 author = 'Fanzhou Kong'
