29789603769b370509290c9d1c4f816ef6bb8e80
//Fanzhou Kong <36810889+FanzhouKong@users.noreply.github.com>
2024-10-02 19:54:13 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import sys
-
+import os
 sys.path.insert(0, os.path.abspath("../../"))
 sys.path.insert(0, os.path.abspath("../../spectral_denoising"))
 project = 'Spectral denoising'
